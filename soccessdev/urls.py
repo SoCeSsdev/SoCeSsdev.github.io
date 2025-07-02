be3222ce239b70f27c6a834e8bfed5798054dc63
Success Agbikimi <faithagbikimi8@gmail.com>
2025-07-02 18:05:07 +0100

    Add new pages and update existing ones

--- soccessdev/urls.py
+++ soccessdev/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for kratosubz_project project.
+URL configuration for soccessdev project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
